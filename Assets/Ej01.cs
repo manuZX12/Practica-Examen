704a1908c9aebc1985b8a34a0f02cafd005c91f3
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-04-08 11:35:43 -0300

    Excersise 01 and 02 made

---
+++ Assets/Ej01.cs
@@ -0,0 +1,23 @@
+﻿//Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour {
+    public int val1;
+    public int val2;
+	void Start () {
+		if (val1 == val2)
+        {
+            Debug.Log("Los valores ingresados son iguales");
+        }
+        else
+        {
+            Debug.Log("Los valores ingresados no son iguales");
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Ej02.cs
@@ -0,0 +1,35 @@
+﻿//Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+//Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour {
+    public int producto1;
+    public int producto2;
+    public int producto3;
+    public int dinerodisponible;
+    int resultante;
+	// Use this for initialization
+	void Start () {
+        resultante = (producto1 + producto2 + producto3);
+        if (resultante > dinerodisponible)
+        {
+            Debug.Log("Los productos superan el monto disponible");
+            resultante = (dinerodisponible - resultante);
+            Debug.Log("A usted le faltan " +resultante+"");
+        }
+        else
+        {
+            Debug.Log("Los productos no superan el monto disponible");
+            resultante = (dinerodisponible - resultante);
+            Debug.Log("A usted le sobran " + resultante + "");
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
